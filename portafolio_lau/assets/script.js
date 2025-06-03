6966f7b3b184640ff1dbc836ad7e4266031c6cd0
lauzarg <lauzarate@mi.unc.edu.ar>
2025-06-02 22:05:33 -0300

    se acomodan los archivos para que funcione el responsive

---
+++ portafolio_lau/assets/script.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function () {
+  const hamburger = document.getElementById('hamburger');
+  const navMenu = document.getElementById('nav-menu');
+
+  hamburger.addEventListener('click', () => {
+    hamburger.classList.toggle('active');
+    navMenu.classList.toggle('active');
+  });
+});
